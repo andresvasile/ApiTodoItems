04cf5aaba0f22d7b184ab960ebda92e367dbda0b
mk5cs <andresvasile.av@gmail.com>
2021-11-18 15:38:59 -0300

    Agregar archivos de proyecto.

---
+++ ApiTodoItems/Persistence/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiTodoItems.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTodoItems.Persistence
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) :base(options)
+        {
+             
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ApiTodoItems/Persistence/AppDbContextSeed.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiTodoItems.Domain.Entities;
+
+namespace ApiTodoItems.Persistence
+{
+    public class AppDbContextSeed
+    {
+        internal static async Task SeedAsync(AplicationDbContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.TodoItems.Any())
+                {
+                    var todoItems = new List<TodoItem>();
+
+                    var todoItem = new TodoItem
+                    {
+                        Description = "Buy groceries",
+                        IsCompleted = true
+                    };
+                    var todoItem2 = new TodoItem
+                    {
+                        Description = "Prepare weekly report",
+                        IsCompleted = false
+                    };
+                    var todoItem3 = new TodoItem
+                    {
+                        Description = "Write to candidates",
+                        IsCompleted = true
+                    };
+
+                    todoItems.Add(todoItem);
+                    todoItems.Add(todoItem2);
+                    todoItems.Add(todoItem3);
+
+                    foreach (var item in todoItems)
+                    {
+                        context.TodoItems.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                var logger = loggerFactory.CreateLogger<AplicationDbContext>();
+                logger.LogError(e.Message);
+            }
+        }
+    }
+}
